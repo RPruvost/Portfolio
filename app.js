3368a43003536149cd4f9a52b863bdda4a47c58a
Avey <romeo.pruvost3007@gmail.com>
2021-04-25 16:38:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// To top Arrow
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 10){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+
+}
